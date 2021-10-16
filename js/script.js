3ee477c1104f051ffaf824a773e0dea879456f28
Gagan Singh <gagansingh.dev@outlook.com>
2021-10-15 23:21:07 -0400

    Added Backdrop display when navbar is open

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+const backdrop = document.getElementById("backdrop");
 const currentYear = document.getElementById("copyright-year");
 
 // Get current year and add it to the DOM in footer section
@@ -10,9 +11,16 @@ const getCurrentYear = new Date().getFullYear();
 currentYear.innerText = getCurrentYear;
 
 // HELPER FUNCTIONS
-const openNavBar = () => navbar.classList.add("showNav");
-const closeNavBar = () => navbar.classList.remove("showNav");
+const openNavBar = () => {
+  navbar.classList.add("showNav");
+  backdrop.classList.add("showBackdrop");
+};
+const closeNavBar = () => {
+  navbar.classList.remove("showNav");
+  backdrop.classList.remove("showBackdrop");
+};
 
 // EVENT LISTENERS
 navBtn.addEventListener("click", openNavBar);
 navClose.addEventListener("click", closeNavBar);
+backdrop.addEventListener("click", closeNavBar);
