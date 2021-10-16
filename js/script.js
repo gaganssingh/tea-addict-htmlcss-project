7d79eeaebf8b928d98005852c747b81f3b772740
Gagan Singh <gagansingh.dev@outlook.com>
2021-10-15 23:09:21 -0400

    Added Navbar HTML & styling with slide-in/out animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-// Get current year and add it to the DOM in footer section
+// DOM SELECTORS
+// Navigation
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
 const currentYear = document.getElementById("copyright-year");
+
+// Get current year and add it to the DOM in footer section
 const getCurrentYear = new Date().getFullYear();
 currentYear.innerText = getCurrentYear;
+
+// HELPER FUNCTIONS
+const openNavBar = () => navbar.classList.add("showNav");
+const closeNavBar = () => navbar.classList.remove("showNav");
+
+// EVENT LISTENERS
+navBtn.addEventListener("click", openNavBar);
+navClose.addEventListener("click", closeNavBar);
